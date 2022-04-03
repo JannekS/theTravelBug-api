d0f6eba66694fad2d257262aa63a9e044eabe4a7
Jannek <janneks@tutanota.com>
2022-04-03 20:11:02 +0200

    Added global error handling function and middleware to set res headers.

--- app.js
+++ app.js
@@ -17,11 +17,32 @@ app.use(express.urlencoded({extended: true})); // TODO: Check if I really need t
 app.use(express.json());    // TODO: Check if I really need this
 app.use(express.static(path.join(__dirname, "public")));
 
+// TODO: check if this is redundant with CORS, or what are the differnces/advantages
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); //include with env.variable
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
 app.use(router);
 
 app.get("/", function (req, res) {
     console.log("Someone requested the server root url...");
     res.send("404 - This page does not exist...");
+});
+
+// Global error handling function
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 sequelize
