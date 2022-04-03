2239f0c7a6fbd4de892b94df5fdfbb2245c9c531
Jannek <janneks@tutanota.com>
2022-04-03 20:28:58 +0200

    added routes for signup, login and new-post.

--- router.js
+++ router.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const postController = require("./controllers/postController");
+const authControler = require('./controllers/auth');
+const isAuth = require('./middleware/is-auth.js');
+const validator = require('./middleware/validators.js');
 
 const router = express.Router();
+
+router.use(express.urlencoded({ extended: true }));
+
+// TODO: write the right business logic for this route
+router.get("/new-post", isAuth, (req, res, next) => {
+    res.json({ message: "You are now authenticated...", userId: req.userId });
+});
+
+router.put("/signup", validator.validateSignup, validator.errorHandler, authControler.signup);
+router.post("/login", validator.validateLogin, validator.errorHandler, authControler.login);
 
 router.get("/posts", postController.servePreviews);
 router.get("/post/:id", postController.servePost);
