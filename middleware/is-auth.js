3a3d75731dc45adb6608dbc69c10b350f441e0c0
Jannek <janneks@tutanota.com>
2022-04-03 20:22:47 +0200

    Added middleware to check for authentication.

---
+++ middleware/is-auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.userId = decodedToken.userId;
+    next();
+  } catch (err) {
+    err.statusCode = 401;
+    if (err.name === 'JsonWebTokenError') {
+      err.message = "Invalid Token.";
+    }
+if (err.name === 'TokenExpiredError') {
+      err.message = "You have been logged out automatically.";
+    }
+    next(err);
+  }
+};
